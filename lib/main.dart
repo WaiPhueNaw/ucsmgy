b8dcc643e527e9e940e97e7505e5b98285fba66c
WaiPhueNaw <waiphuenaw@gmail.com>
2025-08-18 10:26:29 +0630

    wai

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:ucsmgy/pages/home_page.dart';
 
 void main() {
-  
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    )
+  );
 }
 
 
